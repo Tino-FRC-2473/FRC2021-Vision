7a65d31c092c7b652ea64ab7dc00b94781be32c2
Shrinandan-N <emailshri.n@gmail.com>
2020-12-05 10:46:31 -0800

    Add camera calibration files

---
+++ calibrate_camera.py
@@ -0,0 +1,48 @@
+import numpy as np
+import argparse
+import os
+import cv2
+from image_generator import ImageGenerator
+
+parser = argparse.ArgumentParser()
+parser.add_argument("dir", help="directory with photos")
+args = parser.parse_args()
+
+# termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# Squares are 1.25" across
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6 * 7, 3), np.float32)
+objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
+
+# Arrays to store object points and image points from all the images.
+objpoints = []  # 3d point in real world space
+imgpoints = []  # 2d points in image plane.
+
+for image in os.listdir(args.dir):
+    # print(image)
+    generator = ImageGenerator(args.dir + "/" + image)
+    img, _ = generator.generate()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Find the chess board corners
+    ret, corners = cv2.findChessboardCorners(gray, (7, 6), None)
+    # If found, add object points, image points (after refining them)
+    print("object points: ", objp)
+    if ret:
+        objpoints.append(objp)
+        corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+        imgpoints.append(corners)
+        # Draw and display the cornerss
+        cv2.drawChessboardCorners(img, (7, 6), corners2, ret)
+        cv2.imshow('img', img)
+        cv2.waitKey()
+        ret, mtx, dist, rvec, tvec = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+        print("matrix:", mtx)
+        # print("dist:", dist)
+        print("reprojection error:", ret)
+        print("translation vector:", tvec)
+        print("------------")
+
+cv2.destroyAllWindows()
---
+++ video_live_generator.py
@@ -0,0 +1,46 @@
+import cv2
+import numpy as np
+import subprocess
+
+
+class VideoLiveGenerator:
+    def __init__(self, input_port):
+        self.input_port = input_port
+        self.input = cv2.VideoCapture(int(input_port))
+        self.set_camera_settings(str(input_port))
+
+        self.input.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        self.input.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+        frame = self.get_frame()
+        self.SCREEN_HEIGHT, self.SCREEN_WIDTH = frame.shape[:2]
+
+        self.H_FIELD_OF_VIEW = 68.37
+        self.V_FIELD_OF_VIEW = 41.21
+
+    def __enter__(self):
+        return self
+
+    def set_camera_settings(self, camera_port):
+        camera_path = "/dev/video" + camera_port
+
+        try:
+            subprocess.call(["v4l2-ctl", "-d", camera_path, "-c", "exposure_auto=1"])
+            subprocess.call(["v4l2-ctl", "-d", camera_path, "-c", "exposure_absolute=1"])
+        except FileNotFoundError:
+            print("exposure adjustment not completed")
+
+    def get_frame(self):
+        _, frame = self.input.read()
+        frame = np.rot90(frame, 3).copy()
+        return frame
+
+    def get_horizontal_fov(self):
+        return self.H_FIELD_OF_VIEW
+
+    def get_vertical_fov(self):
+        return self.V_FIELD_OF_VIEW
+
+    def generate(self):
+        frame = self.get_frame()
+        return frame, None
