9af2a67f78f3767f45ed1bb4720d19d4d9a23767
Siddarth Mamidanna <siddarth.mamidanna@gmail.com>
2020-12-09 19:46:59 -0800

    Fix the previously listed errors in the calibration code

--- calibrate_camera.py
+++ calibrate_camera.py
@@ -16,6 +16,7 @@ criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 objp = np.zeros((6 * 7, 3), np.float32)
 objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
 
+
 # Arrays to store object points and image points from all the images.
 objpoints = []  # 3d point in real world space
 imgpoints = []  # 2d points in image plane.
--- video_live_generator.py
+++ video_live_generator.py
@@ -9,8 +9,8 @@ class VideoLiveGenerator:
         self.input = cv2.VideoCapture(int(input_port))
         self.set_camera_settings(str(input_port))
 
-        self.input.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-        self.input.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+        self.input.set(cv2.CAP_PROP_FRAME_WIDTH, 1080)
+        self.input.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
         frame = self.get_frame()
         self.SCREEN_HEIGHT, self.SCREEN_WIDTH = frame.shape[:2]
