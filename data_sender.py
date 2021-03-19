4a886ddfe3dbf1ae1e61cf18aac7ce56a9acc377
Shivani Sahni <shivanisahni@Shivanis-MacBook-Pro.local>
2021-03-18 19:30:12 -0700

    fixed Serial port name in data_sender

--- data_sender.py
+++ data_sender.py
@@ -3,11 +3,8 @@ import serial
 class DataSender:
 
     def __init__(self, name="ttyS0", rate=9600):
-        self.s = serial.Serial('/dev'+name, rate)
+        self.s = serial.Serial('/dev/'+name, rate)
 
     def send_data(self, pattern):
         data_to_send = pattern
         self.s.write(bytes("S " + data_to_send + " E", 'utf-8'))  # S is start, E is end. AR is Path A Red, etc
-
-
-
